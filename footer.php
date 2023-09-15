d716b2ed26c9f58b72e9d2e96c876a13c35e4f25
laxatzi <laxatzi@yahoo.com>
2023-09-15 20:03:23 +0300

    INIT-footer.php with footer menu and wp_footer()

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+  <menu>
+    <a href="#twitter">Twitter</a> .
+    <a href="#linkedin">Linkedin</a> .
+    <a href="#youtube">YouTube</a> .
+     <a href="#youtube">Privacy</a> .
+      <a href="#youtube">Terms</a>
+  </menu>
+    <div id="sig">
+    <small>Developed by <span>Lambros Hatzinikolaou</span> &#169; <?php echo wp_kses_post(date('Y')) ?>.
+    All rights reserved.</small>
+   </div>
+</footer>
+<?php
+  wp_footer();
+?>
+</body>
+</html>
