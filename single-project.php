f83869de5702b8935a364249aad849255a0a7af3
laxatzi <laxatzi@yahoo.com>
2023-09-18 12:23:40 +0300

    ADD- code for project posts

---
+++ single-project.php
@@ -0,0 +1,23 @@
+<?php
+
+  get_header();
+?>
+<main>
+  <div class="container">
+<?php
+  while(have_posts()) {
+   the_post(); ?>
+   <div class="the-project">
+      <h3 class="project-title">
+        <?php the_title(); ?>
+        <small><?php echo get_the_date( 'l F j, Y' ); ?></small>
+      </h3>
+    </div>
+   <?php the_content(); ?>
+
+</div>
+</main>
+<?php
+  }
+  get_footer();
+?>
