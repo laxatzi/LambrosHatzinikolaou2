4e4b40acc7c3d23008e7854425691966e2a5d570
laxatzi <laxatzi@yahoo.com>
2023-09-19 16:34:33 +0300

    ADD-page-about.php

---
+++ page-about.php
@@ -0,0 +1,38 @@
+<?php
+  get_header();
+  ?>
+        <div >
+        <main id="contain-about">
+          <img
+            style="max-height: 400px"
+            src="https://via.placeholder.com/200x400.png"
+            alt=""
+          />
+          <div id="p-wrapper">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ad
+              quo similique laboriosam corporis asperiores minima culpa nostrum
+              fuga autem. Veniam maxime <a href="">ipsum pariatur</a> esse aut
+              animi dolorum alias aliquid dolores nobis fuga quis voluptas
+              corrupti eius.
+            </p>
+            <p>
+              Debitis, recusandae nostrum? Maxime recusandae reprehenderit quo
+              vero eius laudantium! Vero, <a href="">deleniti culpa</a>.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, <a href="">deleniti culpa</a> amet
+              consectetur adipisicing elit. Natus, iusto. Officiis.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+              obcaecati illo modi, accusantium, excepturi quae, impedit alias
+              sit possimus eos quaerat quibusdam? Vero suscipit animi est eum
+              nesciunt, corporis veniam!
+            </p>
+          </div>
+        </main>
+      </div>
+  <?php
+  get_footer();
+?>
