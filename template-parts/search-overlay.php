63a480001bdb6478c4ea38133058c2d10b0e2d7a
laxatzi <laxatzi@yahoo.com>
2023-09-19 16:49:10 +0300

    ADD-template part for search overlay

---
+++ template-parts/search-overlay.php
@@ -0,0 +1,13 @@
+  <div class="search-overlay search-overlay--mute">
+    <div class="label-form">
+      <span class="closer">&times;</span>
+      <label for="search">Search what you are looking for:</label>
+      <div class="input-field">
+        <input name="search" id="search" type="search" /> <span>&#128270;</span>
+      </div>
+    </div>
+    <div id="wrapper-spinner">
+      <div class="spinner-div"></div>
+      <div id="search-overlay--search-results" class="search-results--styling"></div>
+    </div>
+   </div>
