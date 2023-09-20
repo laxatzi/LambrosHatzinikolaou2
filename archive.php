5dfbf65e5b73915485d634b550deddf48ab4702e
laxatzi <laxatzi@yahoo.com>
2023-09-20 12:49:27 +0300

    FIX-remove file format from template part

--- archive.php
+++ archive.php
@@ -10,8 +10,9 @@
       <?php
         while(have_posts()) {
           the_post();
-          get_template_part('template-parts/blog-presentation.php');
+          get_template_part('template-parts/blog-presentation');
         }
+
         echo paginate_links();
       ?>
 
