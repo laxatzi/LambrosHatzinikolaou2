27a79a6935403528c3d98881ab38e148f67eb2f0
laxatzi <laxatzi@yahoo.com>
2023-09-20 11:57:58 +0300

    INIT-form-basic template part

---
+++ template-parts/form-basic.php
@@ -0,0 +1,28 @@
+<form action="index.php">
+  <div class="label-form">
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name" required />
+  </div>
+  <div class="label-form">
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" required />
+  </div>
+  <div class="label-form">
+    <label for="subject-line">Send your inquiry or just say hello!</label>
+    <div class="textarea">
+      <input
+        type="text"
+        id="subject-line"
+        name="subject-line"
+        placeholder="Subject line"
+        required
+      />
+    <textarea
+      name="message"
+      rows="5"
+      placeholder="Your message..."
+    ></textarea>
+    </div>
+  </div>
+  <input type="submit" name="submit" value="Send Message" />
+</form>
---
+++ template-parts/newsletter.php
@@ -0,0 +1,11 @@
+<section id="newsletter">
+  <h2>My Weekly Newsletter about Frontend</h2>
+    <small>Subscribe to my newsletter to get useful tips and the week's
+              selection of articles about frontend technologies every
+              Tuesday.
+    </small>
+    <form action="">
+      <input type="email" placeholder="Email" name="email" autocomplete="email"/>
+      <input type="submit" value="Subscribe" />
+    </form>
+</section>
