6490181ec36ccc81645cfc94834ba9493ad42087
laxatzi <laxatzi@yahoo.com>
2023-09-20 13:48:48 +0300

    REFACTOR- make toggling navbar listener to listen to both click and keydown enter events

--- functions.php
+++ functions.php
@@ -43,27 +43,27 @@ function wpb_hook_javascript_footer() {
       const nav = document.querySelector("#js--menu");
       const icon = document.querySelector(".toggle-menu ion-icon");
 
-      toggleMenu.addEventListener(
-        "click",
-        function () {
-          nav.classList.toggle("show");
-          const menuIcon = document.getElementsByTagName("ion-icon")[0];
-          const closeIcon = document.getElementsByTagName("ion-icon")[1];
+       ["click", "keypress"].forEach((ev)=>{
+        toggleMenu.addEventListener(ev, function(e) {
+          if (ev === 'click' || e.key == 'Enter') {
+            nav.classList.toggle("show");
+            const menuIcon = document.getElementsByTagName("ion-icon")[0];
+            const closeIcon = document.getElementsByTagName("ion-icon")[1];
 
           if (menuIcon.classList.contains("show")) {
-            menuIcon.classList.add("close");
-            menuIcon.classList.remove("show");
-            closeIcon.classList.remove("close");
-            closeIcon.classList.add("show");
-          } else {
-            menuIcon.classList.add("show");
-            menuIcon.classList.remove("close");
-            closeIcon.classList.remove("show");
-            closeIcon.classList.add("close");
+              menuIcon.classList.add("close");
+              menuIcon.classList.remove("show");
+              closeIcon.classList.remove("close");
+              closeIcon.classList.add("show");
+           } else {
+             menuIcon.classList.add("show");
+             menuIcon.classList.remove("close");
+             closeIcon.classList.remove("show");
+             closeIcon.classList.add("close");
+           }
           }
-        },
-        false
-      );
+        }, false);
+      });
 
   // smooth scrolling
     const hopLinks = document.querySelectorAll(".hop");
