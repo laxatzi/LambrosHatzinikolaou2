bac9cb8b77022df42ea0c191d534a67a8b72a83d
laxatzi <laxatzi@yahoo.com>
2023-09-20 12:21:24 +0300

    REPLACE-hero section html with hero-section template part

--- front-page.php
+++ front-page.php
@@ -3,30 +3,9 @@
   ?>
     <main>
         <div class="container">
-          <section id="hero">
-            <div class="intro-heading-section">
-              <div class="intro-heading-image">
-                <img
-                  class="intro-heading-img"
-                   src="<?php echo get_template_directory_uri(); ?>/images\aggelikoyla.jpg"
-                  alt="Lambros Hatzinikolaou's cat profile"
-                  title="Aggeliki is a web developer's cat!"
-                />
-              </div>
-              <h1 class="intro-heading">Hi, I'm Lambros</h1>
-            </div>
-            <p>
-              I'm a web developer in Thessaloniki, Greece. This is my tech blog
-              where <a href="<?php echo site_url('/blog') ?>">I write about web development, web design, and my life as a
-              programmer</a>.
-            </p>
-            <p>I've written <a href="#latest-posts" class="hop">a few interesting posts</a> recently.</p>
-            <p>
-              I try to implement what I've learned so building
-              <a href="#latest-projects" class="hop">a lot of projects</a> is part of the
-              process to enhance my learning.
-            </p>
-          </section>
+          <?php
+            get_template_part('template-parts/hero-section');
+          ?>
 
           <section id="newsletter">
             <h2>My Weekly Newsletter about Frontend</h2>
