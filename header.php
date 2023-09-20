e40aa7e57cb4da7b23948cc5e467b6e96b692b93
laxatzi <laxatzi@yahoo.com>
2023-09-20 13:24:09 +0300

    REPLACE html menu with template part menu-basic

--- header.php
+++ header.php
@@ -27,17 +27,8 @@
           <ion-icon name="menu" class="show"></ion-icon>
           <ion-icon name="close" class="close"></ion-icon>
       </div>
-      <menu class="nav" id="js--menu">
-        <a <?php if (is_page('about')) echo 'class="indicator"' ?>   href="<?php echo site_url('/about') ?>">About</a>
-        <a <?php if (get_post_type() == 'post') echo 'class="indicator"' ?> href="<?php  echo site_url('/blog') ?>">Articles</a>
-        <a <?php if (get_post_type() == 'project') echo 'class="indicator"' ?> href="<?php  echo site_url('/projects') ?>">Projects</a>
-          <!--
-            <a href="#books">Books</a>
-            <a href="#now">Now</a>
-          -->
-        <a <?php if (is_page('contact')) echo 'class="indicator"' ?> href="<?php echo site_url('/contact') ?>">Contact</a>
+       <?php
 
-       <div tabindex="0" class="visible search-trigger" aria-description="You can search for your topic of interest either by clicking here or by clicking the keys s + alt">
-         Search <ion-icon name="search" size="small" ></ion-icon> </div>
-      </menu>
+        get_template_part('template-parts/menu-basic');
+       ?>
     </header>
