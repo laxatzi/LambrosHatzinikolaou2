455acba287509499affcda5aaf2e3d44db43df8b
laxatzi <laxatzi@yahoo.com>
2023-09-27 16:00:55 +0300

    CODE - form for searchform.php

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php echo esc_url(site_url('/')) ?> ">
+      <input type="search" name="s" placeholder="Continue Searching?">
+      <input type="submit" value="Search">
+    </form>
