202f82d9f2dfd9ab3dab73d9cfc317cd3c840fc8
laxatzi <laxatzi@yahoo.com>
2023-09-27 13:50:41 +0300

    REFACTOR- add conditional statement to check if there are any results

--- search.php
+++ search.php
@@ -6,13 +6,16 @@
  <h2>Your search query was "<span class='search-query'><?php echo esc_html(get_search_query(false)) ?></span>"</h2>
     <section id="posts">
       <?php
-        while(have_posts()) {
-          the_post();
-          get_template_part('/template-parts/search-content');
-
+         if (have_posts()) {
+          while(have_posts()) {
+            the_post();
+            get_template_part('/template-parts/search-content');
+          }
+          echo paginate_links();
+        } else {
+          echo "<h3 class=\"search-query--heading\">No results much the search!</h3>";
         }
 
-        echo paginate_links();
       ?>
 
     </section>
