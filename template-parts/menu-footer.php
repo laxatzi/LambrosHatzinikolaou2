03ad8e22e99f4a529de060cf153ea92d660bf64c
laxatzi <laxatzi@yahoo.com>
2023-09-28 11:31:14 +0300

    CODE- menu footer

---
+++ template-parts/menu-footer.php
@@ -0,0 +1,8 @@
+  <menu>
+    <a href="#twitter">Twitter</a> .
+    <a href="#linkedin">Linkedin</a> .
+    <a href="#github">GitHub</a> .
+   <a <?php if (is_page('privacy-policy')) echo 'class="indicator"' ?>   href="<?php echo site_url('/privacy-policy') ?>">Privacy</a> .
+   <a <?php if (is_page('terms-of-service')) echo 'class="indicator"' ?>   href="<?php echo site_url('/terms-of-service') ?>">Terms</a>
+  </menu>
+
