651cf65a2ea9ad6e25bcaf4f4e4ba1591335eb67
laxatzi <laxatzi@yahoo.com>
2023-09-28 16:32:53 +0300

    UPDATE- labels in form-basic template

--- template-parts/form-basic.php
+++ template-parts/form-basic.php
@@ -1,15 +1,15 @@
 <form name="contact_form" method="post" action="<?php echo esc_url( get_permalink() ); ?>" enctype="multipart/form-data" autocomplete="off">
 <input type="hidden" name="contact_form">
   <div class="label-form">
-    <label for="name">Name</label>
+    <label for="name"><?php echo esc_html( 'Name', 'LambrosPersonalTheme' ); ?></label>
     <input type="text" id="name" name="name" required />
   </div>
   <div class="label-form">
-    <label for="email">Email</label>
+    <label for="email"><?php echo esc_html( 'Email', 'LambrosPersonalTheme' ); ?></label>
     <input type="email" id="email" name="email" required />
   </div>
   <div class="label-form">
-    <label for="subject-line">Send your inquiry or just say hello!</label>
+    <label for="subject-line"><?php echo esc_html( 'Send your inquiry or just say hello!', 'LambrosPersonalTheme' ); ?></label>
     <div class="textarea">
       <input
         type="text"
