162d4d8640b5f9721e82db233bd7de9d6f155281
laxatzi <laxatzi@yahoo.com>
2023-09-28 12:26:43 +0300

    REFACTOR- replace menu html with menu-footer tempalte part

--- footer.php
+++ footer.php
@@ -1,18 +1,13 @@
 <footer>
-  <menu>
-    <a href="#twitter">Twitter</a> .
-    <a href="#linkedin">Linkedin</a> .
-    <a href="#youtube">YouTube</a> .
-     <a href="#youtube">Privacy</a> .
-      <a href="#youtube">Terms</a>
-  </menu>
-    <div id="sig">
+  <div id="sig">
+    <?php
+        get_template_part('template-parts/menu-footer');
+     ?>
     <small>Developed by <span>Lambros Hatzinikolaou</span> &#169; <?php echo wp_kses_post(date('Y')) ?>.
     All rights reserved.</small>
-   </div>
+  </div>
 </footer>
 <?php
-
   wp_footer();
 ?>
 </body>
