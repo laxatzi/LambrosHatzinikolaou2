860bce12a1bed2fa55aa27c088fe2b92f488c4d0
laxatzi <laxatzi@yahoo.com>
2023-09-28 11:36:06 +0300

    REFACTOR- replace form html with searchform

--- page-search.php
+++ page-search.php
@@ -3,11 +3,9 @@
   while(have_posts()) {
    the_post(); ?>
    <h1> <?php the_title(); ?></h1>
-    <form method="get" id="search" action="<?php echo esc_url(site_url('/')) ?> ">
-      <input type="search" name="s" placeholder="What are you looking for?">
-      <input type="submit" value="Search">
-    </form>
+
    <?php
+   get_search_form();
   }
    get_template_part('template-parts/search-overlay');
     get_footer();
