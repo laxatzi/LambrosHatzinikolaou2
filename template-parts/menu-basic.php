5370ee251dfe4aa6e186284b2bb165956975fe72
laxatzi <laxatzi@yahoo.com>
2023-09-28 11:47:29 +0300

    ADD - href to search page in search link inside the manu basic template

--- template-parts/menu-basic.php
+++ template-parts/menu-basic.php
@@ -8,7 +8,7 @@
           -->
   <a <?php if (is_page('contact')) echo 'class="indicator"' ?> href="<?php echo site_url('/contact') ?>">Contact</a>
 
-  <a tabindex="0" class="visible search-trigger" aria-description="You can search for your topic of interest either by clicking here or by clicking the keys s + alt">
+  <a href="<?php echo esc_url(site_url('/search')) ?>" tabindex="0" class="visible search-trigger" aria-description="You can search for your topic of interest either by clicking here or by clicking the keys s + alt">
          Search <ion-icon name="search" size="small" ></ion-icon>
   </a>
 </menu>
