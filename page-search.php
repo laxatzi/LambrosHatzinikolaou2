51ad240395abeffea9df9e508f879674a057778f
laxatzi <laxatzi@yahoo.com>
2023-09-28 11:28:58 +0300

    STYLES- change heading h2 to h1 in page.php and page-search.php

--- page-search.php
+++ page-search.php
@@ -2,7 +2,7 @@
   get_header();
   while(have_posts()) {
    the_post(); ?>
-   <h2> <?php the_title(); ?></h2>
+   <h1> <?php the_title(); ?></h1>
     <form method="get" id="search" action="<?php echo esc_url(site_url('/')) ?> ">
       <input type="search" name="s" placeholder="What are you looking for?">
       <input type="submit" value="Search">
--- page.php
+++ page.php
@@ -2,7 +2,7 @@
   get_header();
   while(have_posts()) {
    the_post(); ?>
-   <h2> <?php the_title(); ?></h2>
+   <h1> <?php the_title(); ?></h1>
    <h3>Just a reg page</h3>
    <?php the_content(); ?>
    <?php
