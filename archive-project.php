f1f0a35cf3760772b00e2acf1b7e9c3a214f2dec
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-10-22 12:43:58 +0300

    Add archive header with title and description
    Added header section to display archive title and description.

--- archive-project.php
+++ archive-project.php
@@ -4,6 +4,14 @@
 
 <main id="main-content" aria-labelledby="archive-project-title">
   <div class="container">
+    <header class="archive-header">
+      <?php
+      // Title + description (WP will escape these appropriately)
+
+        the_archive_title('<h1>','</h1>');
+        the_archive_description('<p>','</p>');
+      ?>
+    </header>
     <section id="posts">
       <?php
         while(have_posts()) :
