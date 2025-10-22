a48a041e281b2ecafb98fd340c40f746f0f3ce6f
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-10-22 12:56:07 +0300

    Enhance archive.php with ARIA attributes
    Added ARIA attributes and improved header structure.

--- archive.php
+++ archive.php
@@ -1,8 +1,15 @@
 <?php
  get_header();
 ?>
-<main>
+<main id="main-content" aria-labelledby="archive-title">
   <div class="container">
+    <header class="archive-header">
+        <?php
+        // Title + description (WP will escape these appropriately)
+          the_archive_title('<h1>','</h1>');
+          the_archive_description('<p>','</p>');
+        ?>
+      </header>
     <section id="posts">
        <h1><?php the_archive_title(); ?></h1>
        <p><?php the_archive_description(); ?></p>
