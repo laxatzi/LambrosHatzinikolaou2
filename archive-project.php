4a1f10664e9e68877bad313a06608d297268cd0e
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-10-22 12:42:00 +0300

    Refactor loop syntax and improve HTML structure

--- archive-project.php
+++ archive-project.php
@@ -2,21 +2,17 @@
  get_header();
 ?>
 
-  <main id="main-content" aria-labelledby="archive-project-title">
+<main id="main-content" aria-labelledby="archive-project-title">
+  <div class="container">
     <section id="posts">
-       <h1>Projects</h1>
-       <p><?php the_archive_description(); ?></p>
-
       <?php
-        while(have_posts()) {
+        while(have_posts()) :
           the_post();
           get_template_part('template-parts/project-presentation');
-        }
-
-        echo paginate_links();
+        endwhile;
       ?>
-
     </section>
+  </div>
 </main>
 
 <?php
