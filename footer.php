3b9735783ffd78af94f01d4c7c2aeb648c050316
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-10-23 14:02:08 +0300

    Update footer structure and add navigation

--- footer.php
+++ footer.php
@@ -4,10 +4,12 @@
  */
 ?>
 <footer id="colophon" class="site-footer" role="contentinfo">
-  <div id="sig">
-    <?php
-        get_template_part('template-parts/menu-footer');
-     ?>
+  <div id="sig" class="container">
+    <nav class="footer-nav" aria-label="<?php esc_attr_e( 'Footer', 'LambrosPersonalTheme' ); ?>">
+      <?php
+            get_template_part('template-parts/menu-footer');
+        ?>
+    </nav>
     <small>Developed by <span>Lambros Hatzinikolaou</span> &#169; <?php echo wp_kses_post(date('Y')) ?>.
     All rights reserved.</small>
   </div>
