d03550dbfb1a1a6ffb4cf8b89dcb45f46827c0c9
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-10-23 13:56:19 +0300

    Refactor post display structure in index.php

--- index.php
+++ index.php
@@ -8,34 +8,57 @@
       <h1 id="posts-title" class="screen-reader-text">
         <?php esc_html_e('Posts', 'LambrosPersonalTheme'); ?>
       </h1>
-      <?php
-        while(have_posts()) {
-          the_post();
-      ?>
-      <div class="the-post">
-        <div class="the-post_title">
-          <h3 class="posts">
-            <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-            <small><?php echo get_the_date( 'l F j, Y' ); ?></small>
-          </h3>
-        </div>
-        <div class="tags">
-          <ul class="taglist-parent">
-            <li class="taglist tag">
-              <?php echo get_the_category_list(' / ') ?>
-            </li>
-          </ul>
-        </div>
-        <div><?php
-         if (has_excerpt()) {
-            echo get_the_excerpt();
-          } else {
-            echo wp_trim_words(get_the_content(), 26);
-          } ?></div>
-          <a href="<?php the_permalink(); ?>" class="read_more">Read More</a>
-      </div>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+         <article id="post-<?php the_ID(); ?>" <?php post_class('the-post'); ?>>
+           <header class="the-post_title">
+             <h2 class="posts">
+               <a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+                 <?php the_title(); ?>
+               </a>
+             </h2>
+             <small class="the-date">
+               <time datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>">
+                 <?php echo esc_html( get_the_date( 'l F j, Y' ) ); ?>
+               </time>
+             </small>
+            </header>
+
+          <?php
+            $categories = get_the_category_list( ' / ' ); // already escaped HTML list
+            if ( $categories ) :
+          ?>
+            <div class="tags">
+              <ul class="taglist-parent">
+                <li class="taglist tag"><?php echo $categories; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></li>
+              </ul>
+            </div>
+          <?php endif; ?>
+
+          <div class="entry-summary">
+            <?php
+              $excerpt = get_the_excerpt();
+              if ( $excerpt ) {
+                echo wp_kses_post( wpautop( $excerpt ) );
+              } else {
+                echo esc_html( wp_trim_words( wp_strip_all_tags( get_the_content() ), 26, '…' ) );
+              }
+            ?>
+          </div>
+
+          <p>
+            <a class="read_more" href="<?php echo esc_url( get_permalink() ); ?>">
+              <?php esc_html_e('Read more', 'LambrosPersonalTheme'); ?>
+            </a>
+          </p>
+
+        </article>
      <?php
-        }
+       endwhile; else : ?>
+       <p><?php esc_html_e('No posts found.', 'LambrosPersonalTheme'); ?></p>
+
+     <?php endif; ?>
+     <?php
+        
 
         echo paginate_links();
       ?>
