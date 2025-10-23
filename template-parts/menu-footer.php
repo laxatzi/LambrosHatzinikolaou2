a2d8dd456e5da96feadbdb55492d07eeb80d1f6d
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-10-23 14:06:25 +0300

    Refactor footer menu to use unordered list

--- template-parts/menu-footer.php
+++ template-parts/menu-footer.php
@@ -4,11 +4,23 @@
   $linkedin = 'https://www.linkedin.com/in/yourhandle/';
   $github   = 'https://github.com/yourhandle';
 ?>
-<menu>
-    <a href="#twitter">Twitter</a> .
-    <a href="#linkedin">Linkedin</a> .
-    <a href="#github">GitHub</a> .
-   <a <?php if (is_page('privacy-policy')) echo 'class="indicator"' ?>   href="<?php echo site_url('/privacy-policy') ?>">Privacy</a> .
-   <a <?php if (is_page('terms-of-service')) echo 'class="indicator"' ?>   href="<?php echo site_url('/terms-of-service') ?>">Terms</a>
-  </menu>
+<ul class="footer-links">
+  <li><a href="<?php echo esc_url( $twitter ); ?>" target="_blank" rel="me noopener noreferrer">Twitter</a></li>
+  <li><a href="<?php echo esc_url( $linkedin ); ?>" target="_blank" rel="me noopener noreferrer">LinkedIn</a></li>
+  <li><a href="<?php echo esc_url( $github ); ?>" target="_blank" rel="me noopener noreferrer">GitHub</a></li>
+  <li>
+    <a
+      <?php if ( is_page( 'privacy-policy' ) ) echo 'class="indicator"'; ?>
+      href="<?php echo esc_url( site_url( '/privacy-policy' ) ); ?>">
+      <?php esc_html_e( 'Privacy', 'LambrosPersonalTheme' ); ?>
+    </a>
+  </li>
+  <li>
+    <a
+      <?php if ( is_page( 'terms-of-service' ) ) echo 'class="indicator"'; ?>
+      href="<?php echo esc_url( site_url( '/terms-of-service' ) ); ?>">
+      <?php esc_html_e( 'Terms', 'LambrosPersonalTheme' ); ?>
+    </a>
+  </li>
+</ul>
 
