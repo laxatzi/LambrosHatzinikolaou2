4761be47ccd8948994b3e693caa640a5f4ce0306
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-10-31 16:22:51 +0200

    Add comment for search page template
    Added a comment indicating the purpose of the template.

--- page-search.php
+++ page-search.php
@@ -1,5 +1,9 @@
 <?php
-  get_header();
+// Template for the “Search” page (slug: search)
+get_header();
+?>
+<main id="main-content" class="site-main" aria-labelledby="page-title-<?php the_ID(); ?>">
+
   while(have_posts()) {
    the_post(); ?>
    <h1> <?php the_title(); ?></h1>
@@ -8,5 +12,6 @@
    get_search_form();
   }
    get_template_part('template-parts/search-overlay');
+</main>
     get_footer();
   
