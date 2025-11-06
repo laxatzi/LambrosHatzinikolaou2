6e9c5867c4b176716db6aef2fc88b986f8fdea64
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-11-06 12:00:37 +0200

    Wrap contact section in article tag

--- page-contact.php
+++ page-contact.php
@@ -3,7 +3,7 @@
 ?>
 <main id="main-content" class="site-main" aria-labelledby="page-title-<?php the_ID(); ?>">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+  <article id="post-<?php the_ID(); ?>" <?php post_class('page-contact'); ?>>
   <section id="contactme">
     <h2>Why don't you reach out?</h2>
     <p>
@@ -58,6 +58,7 @@
       </div>
     </div>
   </section>
+  </article>
   <?php endwhile; endif; ?>
 </main>
 <?php
