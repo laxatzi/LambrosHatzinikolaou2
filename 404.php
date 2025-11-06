f940e652ba82d6123f2596ab8f76807b498be083
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-11-06 16:11:05 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,12 +4,10 @@ get_header();
 
 <main id="main-content" class="site-main error-404 not-found" aria-labelledby="page-title">
   <div class="container">
-
-    <h1 id="page-title">404</h1>
-    <h2 class="page-subtitle">
-      <span><?php esc_html_e('Oops', 'LambrosPersonalTheme'); ?></span>, 
-      <?php esc_html_e("we haven't found what you're looking for.", 'LambrosPersonalTheme'); ?>
-    </h2>
+    <header class="error-page-header">
+      <h1 id="page-title">404</h1>
+      <h2 class="page-subtitle"><span><?php esc_html_e('Oops', 'LambrosPersonalTheme'); ?></span>, <?php esc_html_e("we haven't found what you're looking for.", 'LambrosPersonalTheme'); ?></h2>
+    </header>
     <p>
         <?php esc_html_e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'LambrosPersonalTheme'); ?>
     </p>
