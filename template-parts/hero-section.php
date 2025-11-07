8ef3f7acfa295f3d2305e2ce5d0da8afe1fd2114
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-11-07 14:49:57 +0200

    remove hard coded content

--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -18,15 +18,5 @@ $img_src = get_theme_file_uri('images/aggelikoyla.jpg');
         <?php echo esc_html__( "Hi, I'm Lambros", 'LambrosPersonalTheme' ); ?>
       </h1>
     </div>
-    <p>
-      I'm a web developer in Thessaloniki, Greece. This is my tech blog
-      where <a href="<?php echo site_url('/blog') ?>">I write about web development, web design, and my life as a
-      programmer</a>.
-    </p>
-    <p>I've written <a href="#latest-posts" class="hop">a few interesting posts</a> recently.</p>
-    <p>
-      I try to implement what I've learned so building
-      <a href="#latest-projects" class="hop">a lot of projects</a> is part of the
-      process to enhance my learning.
-    </p>
+
  </section>
