2b87048021f79a8507d8364fe2646ea195a22a5b
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2025-11-07 14:50:26 +0200

    add comments

--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -18,5 +18,13 @@ $img_src = get_theme_file_uri('images/aggelikoyla.jpg');
         <?php echo esc_html__( "Hi, I'm Lambros", 'LambrosPersonalTheme' ); ?>
       </h1>
     </div>
-
+<?php
+  // Pull the content of the current front page and print it here.
+  // Works whether you're on a static front page or using a custom front-page template.
+  // Fallback content is provided below if no content is found.
+  // $front_id will be 0 if we're on a posts page or similar non-front-page context.
+  // In that case, we check if the front page is set to a static page and use that ID.
+  // This ensures we always get the correct front page content.
+  // Note: This assumes this template part is only used on the front page.
+  // If used elsewhere, additional checks may be needed.
  </section>
