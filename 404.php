3807deab36ce221667f288a8342dd1ef830b7ece
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-01-21 19:07:30 +0200

    Enhance 404 error page with header and message
    Added error message header and explanation text for 404 page.

--- 404.php
+++ 404.php
@@ -13,10 +13,13 @@
 
 <main id="main-content" class="site-main error-404 not-found" aria-labelledby="page-title">
   <div class="container">
+<!-- Error message header -->
+
     <header class="error-page-header">
       <h1 id="page-title">404</h1>
       <h2 class="page-subtitle"><span><?php esc_html_e('Oops', 'LambrosPersonalTheme'); ?></span>, <?php esc_html_e("we haven't found what you're looking for.", 'LambrosPersonalTheme'); ?></h2>
     </header>
+<!-- Explanation text -->
     <p>
         <?php esc_html_e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'LambrosPersonalTheme'); ?>
     </p>
