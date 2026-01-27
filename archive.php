673e979cbe354eac645e432d9ce64daaff9540bf
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-01-27 13:24:03 +0200

    Refactor archive.php for improved structure

--- archive.php
+++ archive.php
@@ -4,33 +4,33 @@
 <main id="main-content" aria-labelledby="archive-title">
   <div class="container">
     <?php if ( have_posts() ) : ?>
-    <header class="archive-header">
+      <header class="archive-header">
         <?php
         // Title + description (WP will escape these appropriately)
           the_archive_title('<h1>','</h1>');
           the_archive_description('<p>','</p>');
         ?>
       </header>
-    <section id="posts">
+      <section id="posts">
       <?php
         while(have_posts()) :
           the_post();
           get_template_part('template-parts/blog-presentation');
         endwhile;        
       ?>
-    </section>
-    <?php
-    // Accessible pagination
-      the_posts_pagination( [
-        'mid_size'           => 1,
-        'prev_text'          => '<span class="screen-reader-text">' . esc_html__( 'Previous', 'LambrosPersonalTheme' ) . '</span><span aria-hidden="true">←</span>',
-        'next_text'          => '<span class="screen-reader-text">' . esc_html__( 'Next', 'LambrosPersonalTheme' ) . '</span><span aria-hidden="true">→</span>',
-        'screen_reader_text' => esc_html__( 'Posts navigation', 'LambrosPersonalTheme' ),
-        'class'              => 'posts-pagination',
-      ] );
-    ?>
+      </section>
+      <?php
+      // Accessible pagination
+        the_posts_pagination( [
+          'mid_size'           => 1,
+          'prev_text'          => '<span class="screen-reader-text">' . esc_html__( 'Previous', 'LambrosPersonalTheme' ) . '</span><span aria-hidden="true">←</span>',
+          'next_text'          => '<span class="screen-reader-text">' . esc_html__( 'Next', 'LambrosPersonalTheme' ) . '</span><span aria-hidden="true">→</span>',
+          'screen_reader_text' => esc_html__( 'Posts navigation', 'LambrosPersonalTheme' ),
+          'class'              => 'posts-pagination',
+        ] );
+      ?>
     <?php else : ?>
-    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+      <?php get_template_part( 'template-parts/content', 'none' ); ?>
     <?php endif; ?>
    </div>
 </main>
