9dc4f1f937bb3593ade035f0808f6cce5b2ca839
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-01-28 15:08:15 +0200

    Refactor footer social links and improve accessibility
    Updated social links section to use template part and improved class attributes for current page indication.

--- template-parts/menu-footer.php
+++ template-parts/menu-footer.php
@@ -1,26 +1,20 @@
 <?php
-  // Configure your social URLs here (or move to Customizer later)
-  $twitter  = 'https://twitter.com/yourhandle';
-  $linkedin = 'https://www.linkedin.com/in/yourhandle/';
-  $github   = 'https://github.com/yourhandle';
+  get_template_part( 'template-parts/social-links' );
 ?>
-<ul class="footer-links">
-  <li><a href="<?php echo esc_url( $twitter ); ?>" target="_blank" rel="me noopener noreferrer">Twitter</a></li>
-  <li><a href="<?php echo esc_url( $linkedin ); ?>" target="_blank" rel="me noopener noreferrer">LinkedIn</a></li>
-  <li><a href="<?php echo esc_url( $github ); ?>" target="_blank" rel="me noopener noreferrer">GitHub</a></li>
+
+<ul class="footer-links legal-links">
   <li>
-    <a
-      <?php if ( is_page( 'privacy-policy' ) ) echo 'class="indicator"'; ?>
+    <a <?php if ( is_page( 'privacy-policy' ) ) echo 'class="indicator" aria-current="page"'; ?>
       href="<?php echo esc_url( site_url( '/privacy-policy' ) ); ?>">
       <?php esc_html_e( 'Privacy', 'LambrosPersonalTheme' ); ?>
     </a>
   </li>
   <li>
-    <a
-      <?php if ( is_page( 'terms-of-service' ) ) echo 'class="indicator"'; ?>
+    <a <?php if ( is_page( 'terms-of-service' ) ) echo 'class="indicator" aria-current="page"'; ?>
       href="<?php echo esc_url( site_url( '/terms-of-service' ) ); ?>">
       <?php esc_html_e( 'Terms', 'LambrosPersonalTheme' ); ?>
     </a>
   </li>
 </ul>
+
 
