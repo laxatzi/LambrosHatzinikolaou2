a93840312a2b821cdb9cd5b4da9f2bd23592ed66
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-01-30 12:53:58 +0200

    Update newsletter.php
    Add ARIA attributes for clarity

--- template-parts/newsletter.php
+++ template-parts/newsletter.php
@@ -2,7 +2,7 @@
   <h2><?php echo esc_html__( 'My Monthly Newsletter about Web Development', 'LambrosPersonalTheme' ); ?></h2>
     <small><?php echo esc_html__( 'Subscribe to my newsletter to get useful tips and a selection of articles about web technologies on the first Monday of every month.', 'LambrosPersonalTheme' ); ?>
     </small>
-    <form action="" method="post">
+    <form action="" method="post" aria-label="Newsletter subscription form">
       <input type="email" placeholder="<?php echo esc_attr__( 'Email', 'LambrosPersonalTheme' ); ?>" name="email" autocomplete="email" required/>
       <label for="newsletter-email" class="screen-reader-text">
         Email address
