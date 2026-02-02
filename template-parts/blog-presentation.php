78a3d125320f9a703101539291df4ba86b5792fe
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-02-02 16:07:09 +0200

    Fix aria-label escaping for 'Read More' link
    Updated the 'Read More' link to ensure the title is escaped properly in the aria-label.

--- template-parts/blog-presentation.php
+++ template-parts/blog-presentation.php
@@ -30,7 +30,10 @@
         } ?>
     </div>
     <div class="read-more-wrapper">
-        <a href="<?php echo esc_url( get_permalink() ); ?>" class="read_more" aria-label="<?php echo esc_attr( sprintf( __( 'Read more about %s', 'LambrosPersonalTheme' ), get_the_title() ) ); ?>"> <?php echo esc_html__( 'Read More', 'LambrosPersonalTheme' ); ?> </a>
+        <a href="<?php echo esc_url( get_permalink() ); ?>" class="read_more" 
+          aria-label="<?php echo esc_attr( sprintf( __( 'Read more about %s', 'LambrosPersonalTheme' ), 
+          esc_html(get_the_title() ) ) ); ?>"> 
+      <?php echo esc_html__( 'Read More', 'LambrosPersonalTheme' ); ?> </a>
     </div>
   </div>
 </article>
