85237661dc7c30263db8956ad60de0d8dc93e13e
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-02-03 13:21:38 +0200

    Add schema.org markup to article element

--- page.php
+++ page.php
@@ -5,7 +5,10 @@ get_header();
 
 <main id="main-content" class="site-main" aria-labelledby="page-title-<?php the_ID(); ?>">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+      itemscope
+      itemtype="https://schema.org/WebPage"
+    >
 
       <header class="entry-header">
         <h1 id="page-title-<?php the_ID(); ?>" class="entry-title"> <?php the_title(); ?> </h1>
