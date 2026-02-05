cc9014e3097285eb42de4266e00c29597a07bb7c
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-02-05 16:01:00 +0200

    Refactor search form layout with new label and div

--- searchform.php
+++ searchform.php
@@ -1,4 +1,7 @@
 <form method="get" id="searchform" action="<?php echo esc_url(site_url('/')) ?> ">
-      <input type="search" name="s" placeholder="Continue Searching?">
-      <input type="submit" value="Search">
-    </form>
+   <div class="search-form">
+    <label for="search-again">Perform a new search:</label>
+    <input id="search-again" type="search" name="s" placeholder="Continue Searching?">
+  </div>
+  <input type="submit" value="Search">
+</form>
