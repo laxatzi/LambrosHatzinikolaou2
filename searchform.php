b2148c835f4a8233339c753705f451633a62fa69
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-02-06 14:52:52 +0200

    Update search input ID and label in searchform.php

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form method="get" id="searchform" action="<?php echo esc_url(site_url('/')) ?> ">
    <div class="search-form">
-    <label for="search-again">Perform a new search:</label>
-    <input id="search-again" type="search" name="s" placeholder="What are you searching for?" aria-label="Search query">
+    <label for="live-search-input">Perform a new search:</label>
+    <input   id="live-search-input" type="search" name="s" placeholder="What are you searching for?" aria-label="Search query">
   </div>
   <input type="submit" value="Search" aria-label="Submit search">
 </form>
