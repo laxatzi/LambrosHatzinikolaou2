8f52eedd5b5aab33013253d860b1e4dc40fd043a
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-02-09 19:49:19 +0200

    Add meta tag for dateModified in single.php
    Added a meta tag for the modified date to improve SEO.

--- single.php
+++ single.php
@@ -13,6 +13,9 @@ while ( have_posts() ) : the_post(); ?>
       <time datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>" itemprop="datePublished">
         <?php echo esc_html( get_the_date() ); ?>
       </time>
+      <!-- Modified date (hidden visually, important for SEO) -->
+      <meta itemprop="dateModified" content="<?php echo esc_attr( get_the_modified_date( DATE_W3C ) ); ?>">
+      
         <?php if ( get_post_type() === 'post' ) : ?>
           <div class="tags">
             <ul class="taglist-parent">
