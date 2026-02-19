aac0dd58567446291f2c33a9565036a448b90a6c
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-02-19 18:39:52 +0200

    Fix formatting in searchform.php

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php echo esc_url(site_url('/')) ?> ">
+<form method="get" id="searchform" action="<?php echo esc_url(site_url( '/' )) ?> ">
    <div class="search-form">
     <label for="live-search-input">Perform a new search:</label>
     <input   id="live-search-input" type="search" name="s" placeholder="What are you searching for?" aria-label="Search query">
