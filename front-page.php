ec04ad36b96079464933dbdabad27d11142b2eec
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-02-19 15:49:16 +0200

    Refactor PHP template parts for consistency

--- front-page.php
+++ front-page.php
@@ -5,16 +5,16 @@
     <div class="dot-bg-section-1"></div>
       <div class="container">
           <?php
-            get_template_part('template-parts/hero-section');
-            get_template_part('template-parts/newsletter');
+            get_template_part( 'template-parts/hero-section' );
+            get_template_part( 'template-parts/newsletter' );
           ?>
     <!-- latest posts -->
           <section id="latest-posts" class="home-section" aria-labelledby="latest-posts-title">
             <div class="blog-intro">
                   <h2 id="latest-posts-title"><?php esc_html_e( 'My latest posts', 'LambrosPersonalTheme' ); ?></h2>
                   <?php
-                    $posts_page_id = (int) get_option('page_for_posts');
-                    $blog_url = $posts_page_id ? get_permalink($posts_page_id) : home_url('/blog/');
+                    $posts_page_id = (int) get_option( 'page_for_posts' );
+                    $blog_url = $posts_page_id ? get_permalink( $posts_page_id ) : home_url( '/blog/' );
                   ?>
                   <a href="<?php echo esc_url( $blog_url ); ?>" class="read_more">
                      <?php esc_html_e( 'View All', 'LambrosPersonalTheme' ); ?>
@@ -29,11 +29,11 @@
                'update_post_meta_cache' => false,
 
             ));
-          if($latestPosts -> have_posts()) :
-              while($latestPosts -> have_posts()) :
+          if( $latestPosts -> have_posts() ) :
+              while( $latestPosts -> have_posts() ) :
                 $latestPosts -> the_post();
 
-                get_template_part('template-parts/blog-presentation');
+                get_template_part( 'template-parts/blog-presentation' );
 
               endwhile;
           else :
@@ -68,18 +68,18 @@
               </a>
             </div>
             <?php
-            $latestProjects = new WP_Query(array(
+            $latestProjects = new WP_Query( array(
               'posts_per_page' => 2,
               'post_type' => 'project',
               'no_found_rows'          => true,
               'update_post_term_cache' => false,
               'update_post_meta_cache' => false,
-            ));
+            ) );
             if ( $latestProjects->have_posts() ) :
               while($latestProjects -> have_posts()) :
                 $latestProjects -> the_post();
 
-                get_template_part('template-parts/project-presentation');
+                get_template_part( 'template-parts/project-presentation' );
               endwhile;
               endif;
               wp_reset_postdata();
