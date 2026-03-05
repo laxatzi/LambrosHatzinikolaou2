2a2cf4635d9a3ff0b38123df3589cded5a421893
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-03-05 15:15:46 +0200

    Fix HTML structure and update header.php

--- header.php
+++ header.php
@@ -11,16 +11,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ) ?>">
+<meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1" >
 
   <?php  wp_head(); ?>
 
 </head>
 <body <?php body_class(); ?>>
-<?php wp_body_open(); 
-  get_template_part( 'template-parts/alerts' );
-?>
+<?php wp_body_open(); ?>
+
 <button
   id="top-button"
   class="back-to-top"
