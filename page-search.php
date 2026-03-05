f94d348bf16839ab3d219e0c3c6085a5c8a879d3
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-03-05 16:11:03 +0200

    Reorganize search form structure and labels

--- page-search.php
+++ page-search.php
@@ -44,13 +44,15 @@ get_header();
     <?php endif; ?>
 
   <section class="search-form-wrapper section" aria-labelledby="search-form-title">
-    <label for="search-type" class="search-type-label"> <?php esc_html_e( 'Filter by type:', 'LambrosPersonalTheme' ); ?> </label> 
-    <select id="search-type" class="search-type"> 
-      <option value="any"><?php esc_html_e( 'All', 'LambrosPersonalTheme' ); ?></option> 
-      <option value="post"><?php esc_html_e( 'Posts', 'LambrosPersonalTheme' ); ?></option> 
-      <option value="page"><?php esc_html_e( 'Pages', 'LambrosPersonalTheme' ); ?></option> 
-      <option value="project"><?php esc_html_e( 'Projects', 'LambrosPersonalTheme' ); ?></option> 
-    </select>
+    <h2 id="search-form-title" class="screen-reader-text"><?php esc_html_e( 'Search this site', 'LambrosPersonalTheme' ); ?></h2>
+    <label for="search-type" class="search-type-label"> <?php esc_html_e( 'Filter by type:', 'LambrosPersonalTheme' ); ?> </label>
+    <select id="search-type" class="search-type">
+      <option value="any"><?php esc_html_e( 'All', 'LambrosPersonalTheme' ); ?></option>
+      <option value="post"><?php esc_html_e( 'Posts', 'LambrosPersonalTheme' ); ?></option>
+      <option value="page"><?php esc_html_e( 'Pages', 'LambrosPersonalTheme' ); ?></option>
+      <option value="project"><?php esc_html_e( 'Projects', 'LambrosPersonalTheme' ); ?></option>
+   </select>
+
     <?php
       // Core search form (already pre-fills the current query and has label)
       get_search_form();
