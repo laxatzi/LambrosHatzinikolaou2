9b4bbb296b00791251acca2451ab5146fc5ff347
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-03-05 16:39:27 +0200

    Fix typo in search results message

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php
  get_header();
 ?>
-<main style="margin-top:50px">
+<main style="margin-top:50px" class="layout__main" aria-labelledby="search-title">
   <div class="container">
   <h2>Your search query was "<span class='search-query'><?php echo esc_html( get_search_query( false ) ); ?></span>"</h2>
    <section id="posts" class="section" aria-labelledby="search-results-title">
@@ -14,7 +14,7 @@
           }
           echo paginate_links();
         } else {
-          echo "<h3 class=\"search-query--heading\">No results much the search!</h3>";
+          echo "<h3 class=\"search-query--heading\">No results mach the search!</h3>";
         }
 
           get_search_form();
