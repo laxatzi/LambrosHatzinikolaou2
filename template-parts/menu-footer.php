ee45e9f406f2093c8a32786234c99bd5d50bdcb5
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-03-16 15:34:05 +0200

    Refactor footer links structure in menu-footer.php

--- template-parts/menu-footer.php
+++ template-parts/menu-footer.php
@@ -2,19 +2,22 @@
   get_template_part( 'template-parts/social-links' );
 ?>
 
-<ul class="footer-links legal-links">
-  <li>
+<ul class="footer-links site-footer__links legal-links nav__list">
+  
+  <li class="nav__item">
     <a <?php if ( is_page( 'privacy-policy' ) ) echo 'class="indicator" aria-current="page"'; ?>
       href="<?php echo esc_url( site_url( '/privacy-policy' ) ); ?>">
       <?php esc_html_e( 'Privacy', 'LambrosPersonalTheme' ); ?>
     </a>
   </li>
+  
   <li>
     <a <?php if ( is_page( 'terms-of-service' ) ) echo 'class="indicator" aria-current="page"'; ?>
       href="<?php echo esc_url( site_url( '/terms-of-service' ) ); ?>">
       <?php esc_html_e( 'Terms', 'LambrosPersonalTheme' ); ?>
     </a>
   </li>
+  
 </ul>
 
 
