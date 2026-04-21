cb476442407b321e7de44dc8e6bcf3bde75edd39
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-04-21 18:05:15 +0300

    Update footer menu item classes for consistency

--- template-parts/menu-footer.php
+++ template-parts/menu-footer.php
@@ -4,14 +4,14 @@
 
 <ul class="site-footer__links legal-links nav__list">
   
-  <li class="nav__item">
+    <li class="site-footer__nav-item">
     <a <?php if ( is_page( 'privacy-policy' ) ) echo 'class="site-footer__indicator site-footer__link" aria-current="page"'; ?>
       href="<?php echo esc_url( site_url( '/privacy-policy' ) ); ?>">
       <?php esc_html_e( 'Privacy', 'LambrosPersonalTheme' ); ?>
     </a>
   </li>
   
-  <li class="nav__item">
+    <li class="site-footer__nav-item">
     <a <?php if ( is_page( 'terms-of-service' ) ) echo 'class="site-footer__indicator site-footer__link" aria-current="page"'; ?>
       href="<?php echo esc_url( site_url( '/terms-of-service' ) ); ?>">
       <?php esc_html_e( 'Terms', 'LambrosPersonalTheme' ); ?>
