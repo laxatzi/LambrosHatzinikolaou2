c6e15151b54e602a9b15b64392e35d13ff9f7a10
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-05-05 17:18:28 +0300

    Refactor hero section image wrapper and attributes

--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -3,20 +3,20 @@
 $img_src = get_theme_file_uri( 'images/aggelikoyla.jpg' );
 ?>
 <section id="hero" class="section" aria-labelledby="intro-heading">
-  
+
   <div class="intro-heading__section">
-    <div class="intro-heading__image">
+    <div class="intro-heading__image-wrapper">
       <img
-        class="intro-heading__img"
-        src="<?php echo esc_url( $img_src ); ?>"        
-       alt="<?php esc_attr_e( 'Portrait of Aggelikoyla', 'LambrosPersonalTheme' ); ?>"
+        class="intro-heading__image"
+        src="<?php echo esc_url( $img_src ); ?>"
+        alt="<?php esc_attr_e( 'Portrait of Aggelikoyla', 'LambrosPersonalTheme' ); ?>"
         title="<?php esc_attr_e( 'Aggeliki is a web developer\'s cat!', 'LambrosPersonalTheme' ); ?>"
         decoding="async"
       />
     </div>
-    
+
     <h1 class="intro-heading">
-      <?php echo esc_html__( "Hi, I\'m Lambros", 'LambrosPersonalTheme' ); ?>
+      <?php echo esc_html__( "Hi, I'm Lambros", 'LambrosPersonalTheme' ); ?>
     </h1>
   </div>
   
