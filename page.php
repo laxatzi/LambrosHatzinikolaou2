1606d43e9bc5f19fb58e469ef64093ebc3284efd
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-05-06 10:17:00 +0300

    Remove 'layout__content' class from main tag

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 
-<main id="main-content" class="layout__main layout__content site-main" aria-labelledby="page-title-<?php the_ID(); ?>">
+<main id="main-content" class="layout__main site-main" aria-labelledby="page-title-<?php the_ID(); ?>">
   <?php 
   
     if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
