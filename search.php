6f386b9bb76dcf163815b350b9cfe3169fa80190
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-05-06 10:19:10 +0300

    Update search results heading to use translation function

--- search.php
+++ search.php
@@ -5,7 +5,7 @@
   <div class="container">
   <h2 id="search-title">Your search query was "<span class='search-query'><?php echo esc_html( get_search_query( false ) ); ?></span>"</h2>
    <section id="posts" class="section" aria-labelledby="search-results-title">
-     <h2 id="search-results-title" class="u-sr-only">
+      <h2 id="search-results-title" class="u-sr-only"><?php esc_html_e( 'Search results', 'LambrosPersonalTheme' ); ?></h2>
      <?php
         if (have_posts()) {
           $has_non_page_results = false;
