71a1694722ee7d66f98d05cdef55a44b775ade4e
Lambros Chatzinikolaou <laxatzi@yahoo.com>
2026-05-06 09:28:21 +0300

    Sanitize menu item labels with esc_html
    Updated the navigation menu to use esc_html for item labels.

--- template-parts/menu-basic.php
+++ template-parts/menu-basic.php
@@ -30,7 +30,7 @@ $links = [
 ];
 ?>
 
-<nav class="site-header__nav site-nav__primary" aria-label="<?php esc_attr_e( 'Primary', 'LambrosPersonalTheme' ); ?>">
+<nav id="js--menu" class="site-header__nav site-nav__primary" aria-label="<?php esc_attr_e( 'Primary', 'LambrosPersonalTheme' ); ?>">
   <ul class="nav nav__list site-nav" id="js--menu">
     <?php foreach ( $links as $item ) : ?>
       <!-- If you ever want to style the active item differently (background, underline, etc.), it’s useful to have a CSS class on that item. Here, we add the class current-menu-item to the <li> of the active page. -->
@@ -41,7 +41,7 @@ $links = [
           class="nav__list-link <?php echo $item['current'] ? 'indicator' : ''; ?>"
           <?php echo $item['current'] ? 'aria-current="page"' : ''; ?>
         >
-          <?php echo $item['label']; ?>
+          <?php echo esc_html($item['label']); ?>
         </a>
       </li>
     <?php endforeach; ?>
